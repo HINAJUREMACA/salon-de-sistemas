b786e208b4eba4a7ce1e65afb0bcfb432e203f21
HINAJUREMACA <51164604+HINAJUREMACA@users.noreply.github.com>
2019-09-25 12:56:21 -0500

    version1.0

---
+++ src/salon/de/sistemas/SalonDeSistemas.java
@@ -0,0 +1,29 @@
+package salon.de.sistemas;
+
+import java.util.Date;
+
+public class SalonDeSistemas {
+
+    private int numerodesillas;
+    private int numerodepuertas;
+    private String modelo;
+    private boolean tieneaire;
+    private int numerodeteclados;
+
+    public void comprarPuertas() {
+        this.numerodepuertas += 1;
+
+    }
+
+    public void comprarPuertas(int nuevonumerodepuertas) {
+        if (nuevonumerodepuertas <= this.numerodepuertas) {
+            System.out.println("El Salon no tiene puertas");
+        } else {
+            this.numerodepuertas = nuevonumerodepuertas;
+            System.out.println("Nuevas puertas: "
+                    + String.valueOf(nuevonumerodepuertas) + " madera");
+        }
+        
+
+    }
+}
