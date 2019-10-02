c51f10b680efe612d8fbc8fd455bca6c9d263f69
HINAJUREMACA <51164604+HINAJUREMACA@users.noreply.github.com>
2019-10-02 13:11:30 -0500

    version1.2

---
+++ src/salon/de/sistemas/Tornillos.java
@@ -0,0 +1,13 @@
+
+
+public class tornillos extends {
+    
+    private int numeroDetuercas;
+
+    public tornillos(int comprarpuertas, int numeroDetuercas) {
+        super(comprarpuertas);
+        this.numeroDetuercas = numeroDetuercas;
+    }
+}
+
+}
